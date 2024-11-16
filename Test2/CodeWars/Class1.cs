3e8578ffe58e1aacb4887698457f41bdab9fe5f2
Ivan Brayko <isbraiko@gmail.com>
2024-11-16 20:11:55 +0300

    [OLMT-1] Решила первую задачу

---
+++ Test2/CodeWars/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeWars;
+
+public static class CodeWars
+{
+    // Url: https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9
+    public static void Task1()
+    {
+        // ршение
+    }
+}
